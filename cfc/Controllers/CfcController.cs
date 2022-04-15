de707345d621d0c55f9785f44daae8f516157925
Ponprabhakar S <Ponprabhakar@theproindia.com>
2022-04-15 19:04:51 +0530

    first commit

---
+++ cfc/Controllers/CfcController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cfc.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CfcController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            return Ok("Welcome to CFC API");
+        }
+    }
+}
